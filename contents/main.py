4905e89cc8566eed23dd7cb1cf78d86985c4644b
VuVanNghia <nghiavu2k2abc@gmail.com>
2024-04-28 17:33:51 +0700

    update main.py

--- contents/main.py
+++ contents/main.py
@@ -8,7 +8,19 @@ workspace = glob.glob(os.path.join(
     root, "**/*.code-workspace"), recursive=True)
 
 
+def shutdown_computer():
+    if os.name == 'nt':
+        os.system('shutdown /s /t 5')
+    # elif os.name == 'posix':
+    #     os.system('sudo shutdown now')
+    else:
+        print("Không thể tắt máy tự động trên hệ điều hành này.")
+
+
 for i in workspace:
     # print(f"🚀 {i}")
     os.chdir(os.path.dirname(i))
     subprocess.run(["git", "push"])
+
+
+# shutdown_computer()
